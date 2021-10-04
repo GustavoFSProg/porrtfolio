ebb3ba2ae9bb932b2a28b7a11f5b79365eff8a4a
Gustavo <gustavo.prog40@gmail.com>
2021-10-04 16:30:04 -0300

    indo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,8 +5,14 @@ import {
   ListContainer,
   NameTitle,
   Image,
+  TitleSou,
+  Linha,
+  TextSou,
+  Redes,
 } from './style'
 import eu from '../../assets/eu.jpg'
+import face from '../../assets/face.jpg'
+import link from '../../assets/linkdin.jpg'
 
 function Home() {
   return (
@@ -17,7 +23,22 @@ function Home() {
           <Image src={eu} style={{ zIndex: '4' }} />
         </ListContainer>
       </ContainerImage>
-      <HabilitiesContainer></HabilitiesContainer>
+      <HabilitiesContainer>
+        <TitleSou>
+          QUEM SOU
+          <Linha />
+        </TitleSou>
+        <TextSou>
+          Sou desenvolvedor Node js e React js, formado no curso de Sistemas de Informação da Escola
+          Técnica da UFRGS em Porto Alegre - RS
+        </TextSou>
+        <Redes>
+          <a href="http://facebook.com">
+            <img src={face} width="30px" alt="facebook" style={{ marginRight: '9px' }} />
+          </a>
+          <img src={link} width="30px" alt="linkdin" />
+        </Redes>
+      </HabilitiesContainer>
     </Container>
   )
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -44,10 +44,40 @@ export const NameTitle = styled.h1`
 export const HabilitiesContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  /* justify-content: center; */
   background: #e6f3ff;
   width: 100vw;
   height: 700px;
   margin-top: 0px;
   z-index: 2;
+`
+export const TitleSou = styled.h1`
+  font-size: 38px;
+  font-family: 'Source Sans Pro', sans-serif;
+  color: black;
+  margin-left: 180px;
+  margin-top: -300px;
+  width: 200px;
+`
+
+export const Linha = styled.div`
+  width: 50px;
+  height: 7px;
+  background: red;
+  margin-top: 20px;
+`
+export const TextSou = styled.h1`
+  font-size: 17px;
+  color: gray;
+  margin-left: -200px;
+  width: 480px;
+  line-height: 27px;
+  margin-top: -32px;
+  text-align: justify;
+`
+export const Redes = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: -350px;
+  margin-left: 40px;
 `
