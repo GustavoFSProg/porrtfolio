f8eb84b85279d0580e83352fa60fa3eb9d2f9eea
Gustavo <gustavo.prog40@gmail.com>
2021-10-05 08:19:19 -0300

    finalmobile

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -111,7 +111,7 @@ export const TitleSou = styled.h1`
   font-family: 'Source Sans Pro', sans-serif;
   color: black;
   margin-left: 60px;
-  margin-top: -50px;
+  margin-top: -40px;
   width: 780px;
 
   /* @media screen and (max-width: 700px) {
