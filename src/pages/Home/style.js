0a6e37c40b13d6580dc4a4ee0359d6f48101ab0c
Gustavo <gustavo.prog40@gmail.com>
2021-10-05 13:47:59 -0300

    fazendo

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -174,7 +174,7 @@ export const ContainerImage = styled.div`
   justify-content: center;
   background: url(${backImage});
   width: 100vw;
-  height: 250px;
+  height: 280px;
 
   /* margin-top: -330px; */
 
