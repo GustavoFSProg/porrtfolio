5cbb8813e154b4525edf59d2a7b730a76c4deb3d
Gustavo <gustavo.prog40@gmail.com>
2021-10-05 19:59:25 -0300

    oiuiu

--- src/pages/Home/style_mobile.js
+++ src/pages/Home/style_mobile.js
@@ -165,11 +165,11 @@ export const ContactContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   /* align-items: center; */
 
-  width: 87%;
+  width: 75%;
 
   height: 450px;
   margin-top: 0px;
-  margin-left: -50px;
+  margin-left: -90px;
 
   @media screen and (min-width: 500px) {
     margin-top: 250px;
