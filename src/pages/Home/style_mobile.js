4f1c82c16d7196279f33602c8a2b99cd11e5cc44
Gustavo <gustavo.prog40@gmail.com>
2021-10-05 19:07:58 -0300

    mmmmmmmm

--- src/pages/Home/style_mobile.js
+++ src/pages/Home/style_mobile.js
@@ -18,7 +18,7 @@ export const ContainerImage = styled.div`
   justify-content: center;
   background: url(${backImage});
   width: 100vw;
-  height: 750px;
+  height: 570px;
 
   @media screen and (min-width: 600px) {
     margin-top: 770px;
