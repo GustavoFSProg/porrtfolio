4e3087bd7b872385841cf10d3276e3d23f892c8a
Gustavo <gustavo.prog40@gmail.com>
2022-03-03 22:18:34 -0300

    feito

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,10 @@ export const Footer = () => (
     <Container>
       <Title>Contato</Title>
       <Paragraph color="#fff">
-        +55 (51) 98457-6589
+        {/* +55 (51) 98457-6589 */}
         <br />
-        <Link target="_blank" href="mailto:gustavosohne38@gmail.com" rel="noreferrer">
-          gustavosohne38@gmail.com
+        <Link target="_blank" href="mailto:@gmail.com" rel="noreferrer">
+          gustavo
         </Link>
       </Paragraph>
     </Container>
